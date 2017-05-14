97f768eb64d8e19a75751fd5304a12162160b07d
Georgi Georgiev <georgiev.georgi93@gmail.com>
2017-05-14 18:18:04 +0200

    add support for cinder trusted filter for LVM drives

--- nova/virt/block_device.py
+++ nova/virt/block_device.py
@@ -370,11 +370,14 @@ class DriverSnapshotBlockDevice(DriverVolumeBlockDevice):
                virt_driver, wait_func=None, do_check_attach=True):
 
         if not self.volume_id:
+            metadata = {}
+            if 'trust:trusted_host' in instance.flavor.extra_specs:
+                metadata['trust:trusted_host'] = instance.flavor.extra_specs['trust:trusted_host']
             av_zone = _get_volume_create_az_value(instance)
             snapshot = volume_api.get_snapshot(context,
                                                self.snapshot_id)
             vol = volume_api.create(context, self.volume_size, '', '',
-                                    snapshot, availability_zone=av_zone)
+                                    snapshot, availability_zone=av_zone, metadata=metadata)
             if wait_func:
                 self._call_wait_func(context, wait_func, volume_api, vol['id'])
 
@@ -394,10 +397,13 @@ class DriverImageBlockDevice(DriverVolumeBlockDevice):
     def attach(self, context, instance, volume_api,
                virt_driver, wait_func=None, do_check_attach=True):
         if not self.volume_id:
+            metadata = {}
+            if 'trust:trusted_host' in instance.flavor.extra_specs:
+                metadata['trust:trusted_host'] = instance.flavor.extra_specs['trust:trusted_host']
             av_zone = _get_volume_create_az_value(instance)
             vol = volume_api.create(context, self.volume_size,
                                     '', '', image_id=self.image_id,
-                                    availability_zone=av_zone)
+                                    availability_zone=av_zone, metadata=metadata)
             if wait_func:
                 self._call_wait_func(context, wait_func, volume_api, vol['id'])
 
